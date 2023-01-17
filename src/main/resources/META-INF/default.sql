ab5d19aa0e2476839ba9b2bab2debfedb233c6bb
freddyway <alf19640408@gmail.com>
2023-01-17 17:49:40 +0100

    [ID_TASK]:[Description]

---
+++ src/main/resources/META-INF/default.sql
@@ -0,0 +1,8 @@
+insert into categorias(nombre) values ('Deportes');
+insert into categorias(nombre) values ('Informatica');
+insert into categorias(nombre) values ('Alimentacion');
+
+insert into productos(nombre,precio,sku,fecha_registro,categoria_id) values ('peras',100,'MKI001',current_date,3);
+insert into productos(nombre,precio,sku,fecha_registro,categoria_id) values ('limones',200,'MKI002',current_date,3);
+insert into productos(nombre,precio,sku,fecha_registro,categoria_id) values ('naranjas',300,'MKI003',current_date,3);
+
